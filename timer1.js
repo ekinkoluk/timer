e7216ed4dc61e03d8dd86bba7a08b6479506f88f
Ekin Koluk <ekinkoluk@gmail.com>
2021-11-02 23:16:58 +0000

    refactored timer1

--- timer1.js
+++ timer1.js
@@ -1,13 +1,32 @@
-const ourArr = process.argv.slice(2);
+let ourArr = process.argv.slice(2);
 //process.stdout.write('\x07');
 
-for(let arr of ourArr) {
-  if(arr > 0){
-  let delay = Number(arr) * 1000;
-  console.log(arr);
-    setTimeout(() => {
-      console.log(delay);
-      process.stdout.write('\x07');
-    }, delay);
+// for(let arr of ourArr) {
+//   if(arr > 0){
+//   let delay = Number(arr) * 1000;
+//   console.log(arr);
+//     setTimeout(() => {
+//       console.log(delay);
+//       process.stdout.write('\x07');
+//     }, delay);
+//   }
+// }
+
+
+// separate logic 
+
+ourArr = ourArr.map(Number);
+
+let alarms = ourArr.map((arr) => arr * 1000);
+
+const beep = () =>  process.stdout.write('\x07');
+
+const beeper= (delay) => {
+  if(!isNaN(delay) && delay >=0){
+    setTimeout(beep,delay);
   }
+}
+
+for(let alarm of alarms ) {
+    beeper(alarm);
 }
